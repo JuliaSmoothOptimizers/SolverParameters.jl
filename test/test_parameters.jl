5c481ab035514b43986ff85e4f1708c09dcecf89
MonssafToukal <32516773+MonssafToukal@users.noreply.github.com>
2022-11-08 17:12:37 -0500

    fix tests not passing in julia 1.6

--- test/test_parameters.jl
+++ test/test_parameters.jl
@@ -1,6 +1,6 @@
 @testset "Parameters" verbose=true begin
     param = Parameter(1.5e-10, RealInterval(0.0,1.0;lower_open=true), "param")
-    @test_throws r"should be in domain" set_value!(param, 0)
+    @test_throws DomainError set_value!(param, 0)
     set_value!(param, 2/3)
     @test value(param) == 2/3
 end
