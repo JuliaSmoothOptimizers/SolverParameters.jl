5b9c75de59de492b88c4328b0b050d6a1cb74515
tmigot <tangi.migot@gmail.com>
2023-04-18 17:11:47 +0200

    Update make.jl to try deploy the doc

--- docs/make.jl
+++ docs/make.jl
@@ -8,19 +8,19 @@ makedocs(;
   doctest = true,
   linkcheck = false,
   strict = false,
-  authors = "Abel Soares Siqueira <abel.s.siqueira@gmail.com> and contributors",
-  repo = "https://github.com/JuliaSmoothOptimizers/SolverParameters.jl/blob/{commit}{path}#{line}",
+  authors = "Monssaf Toukal and contributors",
+  repo = "https://github.com/MonssafToukal/SolverParameters.jl/blob/{commit}{path}#{line}",
   sitename = "SolverParameters.jl",
   format = Documenter.HTML(;
     prettyurls = get(ENV, "CI", "false") == "true",
-    canonical = "https://JuliaSmoothOptimizers.github.io/SolverParameters.jl",
+    canonical = "https://MonssafToukal.github.io/SolverParameters.jl",
     assets = ["assets/style.css"],
   ),
   pages = ["Home" => "index.md", "Reference" => "reference.md"],
 )
 
 deploydocs(;
-  repo = "github.com/JuliaSmoothOptimizers/SolverParameters.jl",
+  repo = "github.com/MonssafToukal/SolverParameters.jl",
   push_preview = true,
   devbranch = "main",
 )
